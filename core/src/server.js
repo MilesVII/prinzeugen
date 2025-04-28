0f52610e5f5477965f7bc14eaed9d8f4837fa2c4
Miles Seventh <milesseventh@gmail.com>
2025-04-29 02:42:50 +0400

    fix bun server

--- core/src/server.js
+++ core/src/server.js
@@ -1,9 +1,15 @@
 import handler from "./main";
 
+const index = Bun.file("../frontend/dist/index.html");
+
 const server = Bun.serve({
 	port: 7780,
-	routes: {
-		"/api": async (request) => {
+	development: {
+		hmr: false
+	},
+	fetch: async (request) => {
+		const url = new URL(request.url);
+		if (url.pathname.startsWith("/api")) {
 			const cors = {
 				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Methods": "POST, OPTIONS",
@@ -34,6 +40,13 @@ const server = Bun.serve({
 			}
 
 			return new Response(null, options);
+		} else {
+			const file = Bun.file(`../frontend/dist${url.pathname}`);
+			if (await file.exists()) {
+			  return new Response(file);
+			}
+
+			return new Response(index);
 		}
 	}
 });
